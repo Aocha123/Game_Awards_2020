8a63694c1aff04d01c9f0b12a55b69c9badfef18
chinpanGX <52692512+chinpanGX@users.noreply.github.com>
2020-02-25 14:20:15 +0900

    最初のプロトタイプの修正版

---
+++ Prototype修正版/ゲーム大賞プロトタイプ/Assets/Script/Block/sidemove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sidemove : MonoBehaviour
+{
+    private int Frame;
+    [SerializeField]
+    private float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Frame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Frame++;
+        transform.Translate(Speed, 0.0f, 0.0f);
+        if (Frame > 100)
+        {
+            Speed *= -1;
+            Frame = 0;
+        }
+    }
+}
